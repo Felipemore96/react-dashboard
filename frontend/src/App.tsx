515031b44e631d96e46ffe8faee4ddcfa1ef0391
Felipe Moreira <felipe.moreiras796@gmail.com>
2025-01-30 10:06:28 +0100

    Intall tailwind css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,30 @@
-import { useEffect, useState } from "react";
-import { fetchSensorData, SensorData } from "./utils/mockApi";
+// import { useEffect, useState } from "react";
+// import { fetchSensorData, SensorData } from "./utils/mockApi";
 
 function App() {
-  const [sensorData, setSensorData] = useState<SensorData[]>([]);
-  const [error, setError] = useState<string | null>(null);
+  // const [sensorData, setSensorData] = useState<SensorData[]>([]);
+  // const [error, setError] = useState<string | null>(null);
 
-  useEffect(() => {
-    const fetchData = () => {
-      fetchSensorData()
-        .then((data) => {
-          setSensorData(data);
-          setError(null); // Clear previous errors
-        })
-        .catch((err) => setError(err.message));
-    };
+  // useEffect(() => {
+  //   const fetchData = () => {
+  //     fetchSensorData()
+  //       .then((data) => {
+  //         setSensorData(data);
+  //         setError(null); // Clear previous errors
+  //       })
+  //       .catch((err) => setError(err.message));
+  //   };
 
-    fetchData(); // Fetch data initially
+  //   fetchData(); // Fetch data initially
 
-    const interval = setInterval(fetchData, 2000); // Fetch new data every 2 seconds
+  //   const interval = setInterval(fetchData, 2000); // Fetch new data every 2 seconds
 
-    return () => clearInterval(interval); // Cleanup on unmount
-  }, []);
+  //   return () => clearInterval(interval); // Cleanup on unmount
+  // }, []);
 
   return (
     <div>
-      <h1>Real-Time Sensor Dashboard</h1>
+      {/* <h1>Real-Time Sensor Dashboard</h1>
 
       {error && <p style={{ color: "red" }}>Error: {error}</p>}
 
@@ -34,7 +34,7 @@ function App() {
             <strong>{sensor.type}:</strong> {sensor.value} ({sensor.timestamp})
           </li>
         ))}
-      </ul>
+      </ul> */}
     </div>
   );
 }
