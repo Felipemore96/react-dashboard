ff4ea5a40c379040dd07e9d939c61e74e0440726
Felipe Moreira <felipe.moreiras796@gmail.com>
2025-02-03 22:44:08 +0100

    Add toggles instead of checkboxes

--- frontend/src/components/SidePanel/SidePanel.tsx
+++ frontend/src/components/SidePanel/SidePanel.tsx
@@ -38,35 +38,47 @@ export function SidePanel({
       <h2 className={styles.title}>Settings</h2>
       <div className={styles.settingsLine}>
         <label>Temperature</label>
-        <input
-          type="checkbox"
-          checked={widgetState.temperature}
-          onChange={() => toggleWidget("temperature")}
-        />
+        <label className={styles.toggleSwitch}>
+          <input
+            type="checkbox"
+            checked={widgetState.temperature}
+            onChange={() => toggleWidget("temperature")}
+          />
+          <span className={styles.slider}></span>
+        </label>
       </div>
       <div className={styles.settingsLine}>
         <label>Humidity</label>
-        <input
-          type="checkbox"
-          checked={widgetState.humidity}
-          onChange={() => toggleWidget("humidity")}
-        />
+        <label className={styles.toggleSwitch}>
+          <input
+            type="checkbox"
+            checked={widgetState.humidity}
+            onChange={() => toggleWidget("humidity")}
+          />
+          <span className={styles.slider}></span>
+        </label>
       </div>
       <div className={styles.settingsLine}>
         <label>Pressure</label>
-        <input
-          type="checkbox"
-          checked={widgetState.pressure}
-          onChange={() => toggleWidget("pressure")}
-        />
+        <label className={styles.toggleSwitch}>
+          <input
+            type="checkbox"
+            checked={widgetState.pressure}
+            onChange={() => toggleWidget("pressure")}
+          />
+          <span className={styles.slider}></span>
+        </label>
       </div>
       <div className={styles.settingsLine}>
         <label>Combined</label>
-        <input
-          type="checkbox"
-          checked={widgetState.combined}
-          onChange={() => toggleWidget("combined")}
-        />
+        <label className={styles.toggleSwitch}>
+          <input
+            type="checkbox"
+            checked={widgetState.combined}
+            onChange={() => toggleWidget("combined")}
+          />
+          <span className={styles.slider}></span>
+        </label>
       </div>
       <div className={styles.intervalLine}>
         <label>Real-Time Interval: {interval / 1000} s</label>
