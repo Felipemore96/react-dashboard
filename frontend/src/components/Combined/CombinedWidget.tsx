d7448cff73dafea57bdf82223b99e626e97168cd
Felipe Moreira <felipe.moreiras796@gmail.com>
2025-02-03 23:01:40 +0100

    Final extra assignment section

--- frontend/src/components/Combined/CombinedWidget.tsx
+++ frontend/src/components/Combined/CombinedWidget.tsx
@@ -9,7 +9,7 @@ import {
   ResponsiveContainer,
 } from "recharts";
 import styles from "./CombinedWidget.module.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNotification } from "../../hooks/useNotification";
 import { SensorData } from "../../utils/mockApi";
 import { COLORS } from "../../constants";
@@ -26,6 +26,10 @@ interface CombinedWidgetProps {
 
 export function CombinedWidget({ data }: CombinedWidgetProps) {
   const { addNotification } = useNotification();
+  const [visibleData, setVisibleData] = useState({
+    temperature: true,
+    humidity: true,
+  });
 
   // Get timestamp 5 minutes ago
   const fiveMinutesAgo = new Date(Date.now() - 5 * 60 * 1000);
@@ -83,35 +87,71 @@ export function CombinedWidget({ data }: CombinedWidgetProps) {
   return (
     <Widget title="Temperature & Humidity" size="large">
       <div className={styles.chartContainer}>
-        <ResponsiveContainer width="100%" height="100%">
-          <LineChart width={400} height={150} data={result}>
-            <CartesianGrid strokeDasharray="3 3" stroke={COLORS.bg} />
-            <XAxis dataKey="time" tick={{ fill: COLORS.bg }} />
-            <YAxis domain={[0, 100]} tick={{ fill: COLORS.bg }} />
-            <Tooltip
-              contentStyle={{
-                backgroundColor: COLORS.bg,
-                borderColor: COLORS.secondary,
-                borderRadius: "8px",
-                color: COLORS.primary,
-              }}
+        <div className={styles.toggleContainer}>
+          <span>Temperature</span>
+          <label className={styles.toggleSwitch}>
+            <input
+              type="checkbox"
+              checked={visibleData.temperature}
+              onChange={() =>
+                setVisibleData((prev) => ({
+                  ...prev,
+                  temperature: !prev.temperature,
+                }))
+              }
             />
-            <Line
-              type="monotone"
-              dataKey="humidity"
-              stroke={COLORS.opposite}
-              strokeWidth={1}
-              dot={{ fill: COLORS.opposite, r: 4 }}
+            <span className={styles.slider}></span>
+          </label>
+          <span>Humidity</span>
+          <label className={styles.toggleSwitch}>
+            <input
+              type="checkbox"
+              checked={visibleData.humidity}
+              onChange={() =>
+                setVisibleData((prev) => ({
+                  ...prev,
+                  humidity: !prev.humidity,
+                }))
+              }
             />
-            <Line
-              type="monotone"
-              dataKey="temperature"
-              stroke={COLORS.bg}
-              strokeWidth={1}
-              dot={{ fill: COLORS.bg, r: 4 }}
-            />
-          </LineChart>
-        </ResponsiveContainer>
+            <span className={styles.slider}></span>
+          </label>
+        </div>
+        <div className={styles.chartContainer}>
+          <ResponsiveContainer width="100%" height="100%">
+            <LineChart width={400} height={150} data={result}>
+              <CartesianGrid strokeDasharray="3 3" stroke={COLORS.bg} />
+              <XAxis dataKey="time" tick={{ fill: COLORS.bg }} />
+              <YAxis domain={[0, 100]} tick={{ fill: COLORS.bg }} />
+              <Tooltip
+                contentStyle={{
+                  backgroundColor: COLORS.bg,
+                  borderColor: COLORS.secondary,
+                  borderRadius: "8px",
+                  color: COLORS.primary,
+                }}
+              />
+              {visibleData.humidity && (
+                <Line
+                  type="monotone"
+                  dataKey="humidity"
+                  stroke={COLORS.opposite}
+                  strokeWidth={1}
+                  dot={{ fill: COLORS.opposite, r: 4 }}
+                />
+              )}
+              {visibleData.temperature && (
+                <Line
+                  type="monotone"
+                  dataKey="temperature"
+                  stroke={COLORS.bg}
+                  strokeWidth={1}
+                  dot={{ fill: COLORS.bg, r: 4 }}
+                />
+              )}
+            </LineChart>
+          </ResponsiveContainer>
+        </div>
       </div>
     </Widget>
   );
